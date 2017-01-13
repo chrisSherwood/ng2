2b1d46d84a82ebb934876213bb72e174486cb550
chrisSherwood <c.a.sherwood@swansea.ac.uk>
2017-01-13 14:17:17 +0000

    changed first-app to dev

---
+++ dev/src/app/databinding/databinding.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-databinding',
+  templateUrl: './databinding.component.html',
+  styleUrls: ['./databinding.component.css']
+})
+export class DatabindingComponent {
+
+  stringInterpolation = 'This is a stringInterpolation';
+  numberInterpolation = 2;
+  
+  onTest() {
+    return false;
+  }
+}
